f40cc6d81e1d1674dad7deb79e51956212b06375
Michael Widjaya <michaeladhipramanaw@gmail.com>
2024-10-22 15:23:02 +0100

    p1, p2

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Import data and WeatherCard here
+import { WeatherCard } from './components/WeatherCard'
+import cities from './data'
+import Location from './components/Location';
 
 
 function App() {
+    const [location, setLocation] = useState("London")
     return (
         <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
+            <h1 className="title">REACTIVE WEATHER</h1>
+            <h3 className="subtitle">Up to the minute weather news</h3>
+            <div>
+                <Location 
+                    data={cities}
+                    location={location}
+                    setLocation={setLocation}
+                />
+            </div>
+            <div className="app">
+                {cities.map((city, index) => (
+                    <WeatherCard
+                        city={city.city}
+                        temperature={city.temperature}
+                        forecast={city.forecast}
+                    />
+                ))}
+
 
 
             </div>
-            
+
         </>
     )
 }
--- components/Location.js
+++ components/Location.js
@@ -4,19 +4,18 @@ import rainy from "../assets/Sunny.svg"
 import cloudy from "../assets/Cloudy.svg"
 import partlyCloudy from "../assets/PartlyCloudy.svg"
 
-
-
-function WeatherCard({ city, temperature, forecast }) {
+export default function Location({ data, location, setLocation }) {
+    const city = data.find((city) => city.city == location)
     return (
         <div className="card">
             <div className="img-container">
                 <img className="card-img-top"
                     src={
-                        forecast == "Sunny" ?
+                        city.forecast == "Sunny" ?
                             sunny
-                            : forecast == "Rainy" ?
+                            : city.forecast == "Rainy" ?
                                 rainy
-                                : forecast == "Cloudy" ?
+                                : city.forecast == "Cloudy" ?
                                     cloudy
                                     : partlyCloudy
 
@@ -24,16 +23,10 @@ function WeatherCard({ city, temperature, forecast }) {
                     alt="Card image cap" id="icon" />
             </div>
             <div class="card-body">
-                <h3 className="card-title">{city}</h3>
-                <h5 className="card-text">{temperature}</h5>
-                <h5 className="card-text">{forecast}</h5>
+                <h3 className="card-title">{city.city}</h3>
+                <h5 className="card-text">{city.temperature}</h5>
+                <h5 className="card-text">{city.forecast}</h5>
             </div>
         </div>
     );
 };
-
-// Export the WeatherCard
-
-module.exports = {
-    WeatherCard
-}
